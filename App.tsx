9ffa6826b7886f11aa439d0602f0c7749666f9a8
CarlosStrife <carlos.striffe@gmail.com>
2023-04-25 17:57:51 -0600

    navegador completo putamadre

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-/**
- * StrifeComponents
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+const Drawer = createDrawerNavigator();
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -25,9 +22,13 @@ function HomeScreen({ navigation }) {
 }
 
 function DetailsScreen({ route, navigation }) {
-  const { itemId, otherParam } = route.params;
+  //const { itemId, otherParam } = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Drawer.Navigator>
+      
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Details" component={DetailsScreen} />
+    {/*<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{color: 'red'}}>Details Screen</Text>
       <Text style={{color: 'red'}}>itemId: {JSON.stringify(itemId)}</Text>
       <Text style={{color: 'red'}}>otherParam: {JSON.stringify(otherParam)}</Text>
@@ -35,7 +36,8 @@ function DetailsScreen({ route, navigation }) {
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
       />
-    </View>
+  </View>*/}
+    </Drawer.Navigator>
   );
 }
 const Stack = createNativeStackNavigator();
--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 1 INSTALAR JAVA 11.0.18 PORQUE ESTA MIERDA NO PUEDE CON UNA MENOR Y LAS MAYORES TIENEN FALLAS CON EL GRADLE NO SE QUE
 2 INSTALAR NODE
 3 INSTALA GIT EN LA COMPU Y CLONAR ESTA MIERDA
+para instalar el navegador de pantallas empezamos aqui:
+https://reactnavigation.org/docs/getting-started
+
+
+#cambia el archivo babel.config.js por esta mierda
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    "react-native-reanimated/plugin",
+  ],
+};
+y resetea esta mierda con 
+npx react-native start --reset-cache
